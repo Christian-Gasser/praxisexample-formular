726133482d79a475d6b0f8e4991c978501d60e45
Christian-Gasser <christian.gasser@lernende.bbw.ch>
2024-03-18 08:08:15 +0100

    a commit

--- src/components/Formular.jsx
+++ src/components/Formular.jsx
@@ -6,6 +6,7 @@ import InputText from './form-elements/InputText.jsx';
 import InputRadio from './form-elements/InputRadio.jsx';
 import DoubleInputField from './form-elements/DoubleInputField.jsx';
 import CheckBox from './form-elements/CheckBox.jsx';
+import FormularInputs from './FormularInput.jsx';
 
 export default function Formular(props) {
     //State
@@ -37,20 +38,37 @@ export default function Formular(props) {
     const updateFood = (e) => setFood(e.target.value);
     const updateAllergies = (e) => setAllergies(e.target.value);
     const updateComment = (e) => setComment(e.target.value);
-    
+
 
     const changeFood = e => {
         setFood(e.target.value)
     }
-    function onSubmit() {
-        console.log('submitted');
-   
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert('Name: ' + prename + ' ' + name + ' v/o ' + ceviName + ' Addresse: ' + address + ', ' + plz + ' ' + city + ' ');
+
     }
     return (
         <div class="container">
-            <form onSubmit={onSubmit()}>
-                <h1>Anmeldung zum Cevi-Lager</h1>
-                <br />
+            <h1>Anmeldung zum Cevi-Lager</h1>
+            <br />
+            <FormularInput
+                updateName={updateName}
+                updatePrename={updatePrename}
+                updateCeviName={updateCeviName}
+                updateAddress={updateAddress}
+                updatePlz={updatePlz}
+                updateCity={updateCity}
+                updateContactName={updateContactName}
+                updateContactPrename={updateContactPrename}
+                updateContactNum={updateContactNum}
+                updateValidation={updateValidation}
+                updateAllergies={updateAllergies}
+            ></FormularInput>
+
+            <form onSubmit={handleSubmit}>
+
+
 
                 {/*Teilnehmer*/}
                 <div class="row gy-2">
@@ -59,7 +77,7 @@ export default function Formular(props) {
                     <InputField type="text" placeholder="Vorame" name="prename" giveback={updatePrename}></InputField>
                     <InputField type="text" placeholder="Ceviname" name="ceviname" giveback={updateCeviName}></InputField>
                     <InputField type="text" placeholder="Addresse" name="addresse" giveback={updateAddress}></InputField>
-                    <DoubleInputField 
+                    <DoubleInputField
                         type1="text" placeholder1="PLZ" name1="plz" giveback1={updatePlz}
                         type2="text" placeholder2="Ort" name2="city" giveback2={updateCity}
                     ></DoubleInputField>
@@ -95,7 +113,7 @@ export default function Formular(props) {
                 {/*Bemerkungen */}
                 <div class="row gy-2">
                     <h3>Bemerkungen</h3>
-                <InputText placeholder="Krankheiten, regelmässige Medikamente, etc." name="comment" giveback={updateComment}></InputText>
+                    <InputText placeholder="Krankheiten, regelmässige Medikamente, etc." name="comment" giveback={updateComment}></InputText>
                 </div>
                 <br />
                 <br />
